aad8b0e1a72805593833083ac12646be92724a8d
boshify <140970186+boshify@users.noreply.github.com>
2023-12-30 18:02:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,51 @@
 import streamlit as st
 import pandas as pd
+import requests
+import json
 
-# Function to add new columns with placeholder text
-def process_file(file):
+# Function to perform a search using the Google Custom Search API
+def search(query, api_key, cse_id, **kwargs):
+    url = "https://www.googleapis.com/customsearch/v1"
+    params = {
+        'q': query,
+        'key': api_key,
+        'cx': cse_id,
+    }
+    params.update(kwargs)
+    response = requests.get(url, params=params)
+    return json.loads(response.text)
+
+# Function to process the file and add new columns with search result titles
+def process_file(file, api_key, cse_id):
     df = pd.read_csv(file)
-    df['Content Type'] = 'banana'  # Adding 'Content Type' column
-    df['SERP Title 1'] = 'banana'  # Adding 'SERP Title 1' column
-    df['SERP Title 2'] = 'banana'  # Adding 'SERP Title 2' column
-    df['SERP Title 3'] = 'banana'  # Adding 'SERP Title 3' column
-    df['Bold Text'] = 'banana'     # Adding 'Bold Text' column
+
+    # Add new columns for search result titles
+    df['SERP Title 1'] = ''
+    df['SERP Title 2'] = ''
+    df['SERP Title 3'] = ''
+
+    # Iterate over each row and perform search
+    for index, row in df.iterrows():
+        query = row['C']  # Using the search queries from column C
+        results = search(query, api_key, cse_id)
+
+        # Extract and assign the titles of the top 3 search results
+        for i in range(3):
+            if results.get('items') and len(results['items']) > i:
+                df.at[index, f'SERP Title {i+1}'] = results['items'][i].get('title', '')
+
     return df
 
 # Streamlit app layout
 def main():
     st.title("Spreadsheet Processor")
+    api_key = st.text_input("Enter your Google API Key")
+    cse_id = st.text_input("Enter your Custom Search Engine ID")
 
     uploaded_file = st.file_uploader("Upload your file", type=["csv"])
 
-    if uploaded_file is not None:
-        processed_data = process_file(uploaded_file)
+    if uploaded_file is not None and api_key and cse_id:
+        processed_data = process_file(uploaded_file, api_key, cse_id)
 
         st.write("Processed Data:")
         st.write(processed_data)
