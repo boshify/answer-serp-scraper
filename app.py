0aa75d61dba916b8fc20935bad901290c6386bb9
boshify <140970186+boshify@users.noreply.github.com>
2023-12-30 17:23:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,14 @@
 import streamlit as st
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
 
-# Function to perform Google search - Replace with your actual API call
-def google_search(query, api_key):
-    # Here, you'll implement your Google Search API call
-    # Return the top 3 SERP titles and the HTML content of the search result page
-    # For now, these are placeholders
-    return ['Mock Title 1', 'Mock Title 2', 'Mock Title 3'], "<html><body><strong>Mock Bold Text</strong></body></html>"
-
-# Function to extract bold text from HTML content
-def extract_bold_text(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    bold_texts = [elem.get_text() for elem in soup.find_all(['strong', 'b'])]
-    return ' '.join(bold_texts)  # Concatenate all bold texts
-
-# Function to process the uploaded file
-def process_file(file, google_api_key):
+# Function to add new columns with placeholder text
+def process_file(file):
     df = pd.read_csv(file)
-    
-    for idx, row in df.iterrows():
-        query = row['Question']  # Assuming 'Question' is the column to use for search
-        serp_titles, html_content = google_search(query, google_api_key)
-
-        # Update dataframe with mock data - Replace with your logic
-        df.at[idx, 'Content Type'] = 'Mock Content Type'
-        df.at[idx, 'SERP Title 1'], df.at[idx, 'SERP Title 2'], df.at[idx, 'SERP Title 3'] = serp_titles
-        df.at[idx, 'Bold Text'] = extract_bold_text(html_content)
-
+    df['Content Type'] = 'banana'  # Adding 'Content Type' column
+    df['SERP Title 1'] = 'banana'  # Adding 'SERP Title 1' column
+    df['SERP Title 2'] = 'banana'  # Adding 'SERP Title 2' column
+    df['SERP Title 3'] = 'banana'  # Adding 'SERP Title 3' column
+    df['Bold Text'] = 'banana'     # Adding 'Bold Text' column
     return df
 
 # Streamlit app layout
@@ -38,14 +18,12 @@ def main():
     uploaded_file = st.file_uploader("Upload your file", type=["csv"])
 
     if uploaded_file is not None:
-        # Read API key from Streamlit secrets (or another secure source)
-        google_api_key = st.secrets["GOOGLE_API_KEY"]
-
-        processed_data = process_file(uploaded_file, google_api_key)
+        processed_data = process_file(uploaded_file)
 
         st.write("Processed Data:")
         st.write(processed_data)
 
+        # Download button
         st.download_button(
             label="Download processed data",
             data=processed_data.to_csv(index=False),
