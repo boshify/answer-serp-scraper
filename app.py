fdb6dec990c004163d97a50e1d67b64e0ac64451
boshify <140970186+boshify@users.noreply.github.com>
2023-12-30 17:13:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,35 @@
-import os
 import streamlit as st
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-import json
-import re
 
-# Set Streamlit page configuration
-st.set_page_config(page_title="Answer Socrates Helper", page_icon="🔍", layout="wide")
+# Function to add new columns with placeholder text
+def process_file(file):
+    df = pd.read_csv(file)
+    df['Content Type'] = 'banana'  # Adding 'Content Type' column
+    df['SERP Title 1'] = 'banana'  # Adding 'SERP Title 1' column
+    df['SERP Title 2'] = 'banana'  # Adding 'SERP Title 2' column
+    df['SERP Title 3'] = 'banana'  # Adding 'SERP Title 3' column
+    df['Bold Text'] = 'banana'     # Adding 'Bold Text' column
+    return df
 
-# Fetch API key and CSE ID from Streamlit secrets
-api_key = st.secrets["GOOGLE_API_KEY"]
-cse_id = st.secrets["CUSTOM_SEARCH_ENGINE_ID"]
+# Streamlit app layout
+def main():
+    st.title("Spreadsheet Processor")
 
-def google_search(query, api_key, cse_id, num=3):
-    url = "https://www.googleapis.com/customsearch/v1"
-    params = {'q': query, 'key': api_key, 'cx': cse_id, 'num': num}
-    response = requests.get(url, params=params)
-    return json.loads(response.text)
+    uploaded_file = st.file_uploader("Upload your file", type=["csv"])
 
-def extract_bold_text(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    bold_texts = soup.find_all(['strong', 'b'])
-    return [text.get_text().strip() for text in bold_texts]
+    if uploaded_file is not None:
+        processed_data = process_file(uploaded_file)
 
-@st.cache(allow_output_mutation=True)
-def process_queries(uploaded_file, api_key, cse_id):
-    df = pd.read_csv(uploaded_file)  # Assuming CSV, modify for Excel if needed
-    queries = df['Query']  # Replace 'Query' with the actual column name in your CSV
+        st.write("Processed Data:")
+        st.write(processed_data)
 
-    results = []
-    for query in queries:
-        search_results = google_search(query, api_key, cse_id)
-        top_3_links = [item['link'] for item in search_results.get('items', [])[:3]]
-        bold_text = []
-        for link in top_3_links:
-            try:
-                page_content = requests.get(link).text
-                bold_text += extract_bold_text(page_content)
-            except Exception as e:
-                print(f"Error fetching bold text from {link}: {e}")
-        # Guess content type here, currently placeholder
-        content_type_guess = "Article"  # Placeholder, implement actual guessing logic
-        results.append([query] + bold_text + top_3_links + [content_type_guess])
+        # Download button
+        st.download_button(
+            label="Download processed data",
+            data=processed_data.to_csv(index=False),
+            file_name="processed_data.csv",
+            mime="text/csv",
+        )
 
-    columns = ['Query', 'Bold Text 1', 'Bold Text 2', 'Bold Text 3', 'Link 1', 'Link 2', 'Link 3', 'Content Type Guess']
-    return pd.DataFrame(results, columns=columns)
-
-st.title("Answer Socrates Helper")
-
-uploaded_file = st.file_uploader("Upload the Answer Socrates Sheet", type=['csv'])
-
-if uploaded_file is not None:
-    result_df = process_queries(uploaded_file, api_key, cse_id)
-    st.dataframe(result_df)
-
-    csv_result = result_df.to_csv(index=False).encode('utf-8')
-    st.download_button(label="Download Results", data=csv_result, file_name="socrates_results.csv", mime="text/csv")
+if __name__ == "__main__":
+    main()
